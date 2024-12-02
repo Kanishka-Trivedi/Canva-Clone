211f7c964deb14dbddfcfecc2b7dc58164059722
Kanishka-Trivedi <kally651997@gmail.com>
2024-12-02 16:38:14 +0530

    Js Dom and event applied for a form returning the value form submitted

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const button = document.getElementById('b1');
+const text = document.getElementById('t1');
+
+button.addEventListener('click', ()=>{
+    text.value = "Form Submitted!";
+   event.preventDefault();
+});
